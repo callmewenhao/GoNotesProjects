7008ff39d01212a062a2f518b8e823a4957e1ad7
zhaowenhao <54170620+callmewenhao@users.noreply.github.com>
2023-10-04 08:55:07 +0800

    update

--- 数组/map.go
+++ 数组/map.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
 func main() {
 	//temperature := map[string]int{
@@ -76,23 +73,33 @@ func main() {
 	//	fmt.Printf("%v: %v\n", g, temperature)
 	//}
 
-	temperature := []float64{
-		-28.0, 32.0, -31.0, -29.0, -23.0, -29.0, -28.0, -33.0,
-	}
+	//temperature := []float64{
+	//	-28.0, 32.0, -31.0, -29.0, -23.0, -29.0, -28.0, -33.0,
+	//}
+	//
+	//set := make(map[float64]bool)
+	//for _, t := range temperature {
+	//	set[t] = true
+	//}
+	//if set[-28.0] {
+	//	fmt.Println("set member!")
+	//}
+	//fmt.Println(set)
+	//
+	//unique := make([]float64, 0, len(set))
+	//for t := range set {
+	//	unique = append(unique, t)
+	//}
+	//sort.Float64s(unique) // 由小到大排序
+	//fmt.Println(unique)
 
-	set := make(map[float64]bool)
-	for _, t := range temperature {
-		set[t] = true
-	}
-	if set[-28.0] {
-		fmt.Println("set member!")
-	}
-	fmt.Println(set)
+	// 是否含有这个key
+	cnt := make(map[int]int)
+	cnt[0] = 1
+	cnt[1] = 1
+	cnt[2] = 1
 
-	unique := make([]float64, 0, len(set))
-	for t := range set {
-		unique = append(unique, t)
-	}
-	sort.Float64s(unique) // 由小到大排序
-	fmt.Println(unique)
+	a, b := cnt[3]
+	fmt.Println(a, b, cnt)
+
 }
